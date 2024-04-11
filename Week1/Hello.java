66704c74a8fa736fc68a8e5f097d2e8d26318bf0
SambandhaRai <ginny_30@icloud.com>
2024-04-11 13:11:48 +0545

    Task 3

--- Week1/Hello.java
+++ Week1/Hello.java
@@ -11,6 +11,29 @@ public class Hello {
               I am on module ST4003CEM
               System.out.print (not println) and produces the same output. */
         System.out.print("This is my first program\nI am on module ST4003CEM");
+        
+        /* 3. Write a program that prints the following output:
+
+              A "quoted" String is
+
+              'much' better if you learn
+
+              the rules of "escape sequences."
+
+              Also, "" represents an empty
+
+              String. Don't forget: use \"
+
+              instead of " !
+
+              '' is not the same as " */
+        System.out.println("\nA \"quoted\" String is");
+        System.out.println("'much' better if you learn");
+        System.out.println("the rules of \"escape sequences\"");
+        System.out.println("Also, \"\" represents an empty");
+        System.out.println("String. Don't forget: use \"\"");
+        System.out.println("instead of \" !");
+        System.out.println("'' is not the same as \" */");
     }
-    
+
 }
